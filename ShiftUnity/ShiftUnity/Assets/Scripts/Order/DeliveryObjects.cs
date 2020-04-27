f64a6b1db9147ea614bdafd609adb4f32aa4980a
AccidentallySentient <57410460+AccidentallySentient@users.noreply.github.com>
2020-04-27 12:48:41 -0400

    Obastcles
    obstacle nodes can be placed anywhere and randomly spawn 1 of 3 different obastcles.

---
+++ ShiftUnity/ShiftUnity/Assets/Scripts/Order/DeliveryObjects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryObjects : MonoBehaviour
+{
+    public enum CollectibleType
+    {
+        T1,T2,T3
+    }
+
+    public static CollectibleType collectibleType;
+}
---
+++ ShiftUnity/ShiftUnity/Assets/Scripts/Order/PickUpSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpawn : MonoBehaviour
+{
+    public GameObject[] nodes;
+
+    public Transform GetNodeTransform(int id)
+    {
+        return nodes[id].transform;
+    }
+
+    public void Activate(int id)
+    {
+        nodes[id].SetActive(true);
+    }
+    public void Deactivate(int id)
+    {
+        nodes[id].SetActive(false);
+    }
+}
