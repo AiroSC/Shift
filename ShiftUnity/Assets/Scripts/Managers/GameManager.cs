cba8e6fec2bd2b1eae48d8f44a34d7c63bf5cdc6
patelsunny74 <patelsunny74sp@gmail.com>
2020-05-12 13:08:53 -0400

    Adding Quality Bar

--- ShiftUnity/Assets/Scripts/Managers/GameManager.cs
+++ ShiftUnity/Assets/Scripts/Managers/GameManager.cs
@@ -21,17 +21,18 @@ public class GameManager : MonoBehaviour
     private static GameManager instance = null;
     [SerializeField]
     float time;
+    [SerializeField]
     TextMeshProUGUI timer;
+    [SerializeField]
     TextMeshProUGUI score;
+    [SerializeField]
     TextMeshProUGUI speed;
     static int earned;
     int mins;
     int secs;
+    [SerializeField]
     Slider qualitybar;
     int quality;
-
-
-
     private const float gametime = 120.0f;
 
     void Awake()
@@ -45,83 +46,72 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject); //Set as do not destroy
         }
     }
-
     // Start is called before the first frame update
     void Start()
     {
-        quality = 100;
-        if(time == 0)
+        if (quality <= 0)
+            quality = 100;
+        if(time <= 0)
             time = gametime;
         earned = 0;
+        StartCoroutine(UpdateQualityBar());
 
-        timer = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
-        score = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
-        speed = GameObject.Find("Speed").GetComponent<TextMeshProUGUI>();
-        qualitybar = GameObject.Find("QualityBar").GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "Game")
+        if (SceneManager.GetActiveScene().name == "Sunny")
         {
-            
-
+            //timer = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
+            //score = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+            //speed = GameObject.Find("Speed").GetComponent<TextMeshProUGUI>();
+            //qualitybar = GameObject.Find("QualityBar").GetComponent<Slider>();
             time -= Time.deltaTime;
             UpdateLevelTimer(time);
-            StartCoroutine(UpdateQualityBar(quality));
-
             score.text = "Earned: " + earned.ToString();
             speed.text = "Speed: " + Mathf.RoundToInt(GameObject.FindGameObjectWithTag("Player").GetComponent<CarController>().currentSpeed).ToString();
-            quality -= 5;
-
-            UpdateQualityBar(quality);
-
+            qualitybar.value = quality;
             Debuginfo();
         }
         if (SceneManager.GetActiveScene().name == "GameOver")
         {
-            score = GameObject.Find("EndScore").GetComponent<TextMeshProUGUI>();
+            //score = GameObject.Find("EndScore").GetComponent<TextMeshProUGUI>();
             score.text = earned.ToString();
         }
-
-
     }
-
     private void Debuginfo()
     {
         Debug.Log("time:" + time);
         Debug.Log("score:" + earned);
         Debug.Log("speed:" + Mathf.RoundToInt(GameObject.FindGameObjectWithTag("Player").GetComponent<CarController>().currentSpeed));
         Debug.Log("quality:" + quality);
-
     }
-
-    IEnumerator UpdateQualityBar(int q)
+    IEnumerator UpdateQualityBar()
     {
-        qualitybar.value = q;
-        yield return null;
+        while (true)
+        {
+            quality -= 5;
+            yield return new WaitForSeconds(1.0f);
+        }
+            
     }
     public void UpdateLevelTimer(float totalSeconds)
     {
         int minutes = Mathf.FloorToInt(totalSeconds / 60f);
         int seconds = Mathf.RoundToInt(totalSeconds % 60f);
-
         string formatedSeconds = seconds.ToString();
-
         if (seconds == 60)
         {
             seconds = 0;
             minutes += 1;
         }
-
         timer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
         if(minutes <=0 && seconds <= 0)
         {
             SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
         }
     }
-
     public void Earned(DeliveryObjects.CollectibleType type)
     {
         switch (type)
@@ -139,7 +129,6 @@ public class GameManager : MonoBehaviour
                 break;
         }
     }
-
     public int Score
     {
         get
